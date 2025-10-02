0f6101f24fc4a328a8e2c38baef824ce7df7b9eb
Vollkornaffe <vollkornaffe@gmail.com>
2025-10-02 20:17:39 +0200

    feat: improve error messsage for simulation failure

--- rust/crates/core/src/simulation/error_messages.rs
+++ rust/crates/core/src/simulation/error_messages.rs
@@ -1,4 +1,7 @@
 pub const SAMPLING_FAILED: &'static str = "Object could not be sampled.\n\
 This is most likely because the volume is negative or tiny relative to the resolution.\n\
-Please check the normals and consider increasing the resolution.\n\
+Please check the normals and consider increasing the resolution by reducing 'Grid Node Size' or 'Particle Factor'.\n\
 (To increase sample density)";
+pub const INVERTED_PARTICLE: &'static str = "A particle collapsed or inverted.\n\
+This is most likely because the 'Time Step' is too large or because colliders are crushing particles.\n\
+Please try to reduce the 'Time Step' by half (repeatedly) until stability is achieved, then increase cautiously to regain performance.";
