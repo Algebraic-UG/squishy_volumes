f68382adaa70f1f6b8b0c0bb5c8365c963b65900
Vollkornaffe <vollkornaffe@gmail.com>
2025-10-17 21:59:37 +0200

    feat: refactor and add fluid speed of sound

--- rust/crates/core/src/simulation/state/limit_time_step.rs
+++ rust/crates/core/src/simulation/state/limit_time_step.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use nalgebra::Vector3;
+use nalgebra::{Matrix3, Vector3};
 use squishy_volumes_api::T;
 
 const TIME_STEP_HISTORY_LENGTH: usize = 10;
@@ -8,7 +8,9 @@ use crate::{
     math::SINGULAR_VALUE_SEPARATION,
     simulation::{
         elastic::{
+            first_piola_stress_inviscid_svd_in_diagonal_space,
             first_piola_stress_neo_hookean_svd_in_diagonal_space,
+            second_derivative_inviscid_svd_in_diagonal_space,
             second_derivative_neo_hookean_svd_in_diagonal_space,
         },
         particles::ParticleParameters,
@@ -18,13 +20,20 @@ use crate::{
 use super::{PhaseInput, State, profile};
 
 impl State {
-    // Effective time step restrictions for explicit MPM simulation 4.1 Sound Speed
-    // Stability analysis of explicit MPM, Technical document 3.12
     pub(super) fn limit_time_step_before_force(self, phase_input: &mut PhaseInput) -> Result<Self> {
         profile!("limit_time_step_before_force");
+        phase_input.time_step_by_sound = self.limit_time_step_by_speed_of_sound(phase_input);
+        phase_input.time_step_by_isolated = self.limit_time_step_by_isolated_particles(phase_input);
+
+        apply_limit(phase_input);
+        Ok(self)
+    }
+
+    // Effective time step restrictions for explicit MPM simulation 4.1 Sound Speed
+    fn limit_time_step_by_speed_of_sound(&self, phase_input: &mut PhaseInput) -> Option<T> {
+        profile!("limit_time_step_by_speed_of_sound");
         let grid_node_size = phase_input.setup.settings.grid_node_size;
-        phase_input.time_step_by_sound = self
-            .particles
+        self.particles
             .parameters
             .iter()
             .zip(self.particles.position_gradients.iter())
@@ -32,140 +41,120 @@ impl State {
             .zip(self.particles.initial_volumes.iter())
             .map(
                 |(((parameters, position_gradient), mass), initial_volume)| {
+                    let s = position_gradient.svd(false, false).singular_values;
+
+                    let j = s.product();
+
+                    // we need to use L'Hôpital in this case
+                    let xy_close = (s.x - s.y).abs() < SINGULAR_VALUE_SEPARATION;
+                    let yz_close = (s.y - s.z).abs() < SINGULAR_VALUE_SEPARATION;
+                    let zx_close = (s.z - s.x).abs() < SINGULAR_VALUE_SEPARATION;
+
+                    let first: Vector3<T>;
+                    let second: Matrix3<T>;
+
                     match parameters {
                         ParticleParameters::Solid {
                             mu,
                             lambda,
-                            viscosity,
-                            sand_alpha,
+                            viscosity,     // viscosity is TODO
+                            sand_alpha: _, // this shouldn't lower the required timestep
                         } => {
-                            let s = position_gradient.svd(false, false).singular_values;
-                            let first = first_piola_stress_neo_hookean_svd_in_diagonal_space(
+                            first = first_piola_stress_neo_hookean_svd_in_diagonal_space(
                                 *mu, *lambda, &s,
                             );
-                            let second = second_derivative_neo_hookean_svd_in_diagonal_space(
+                            second = second_derivative_neo_hookean_svd_in_diagonal_space(
                                 *mu, *lambda, &s,
                             );
-
-                            let j = s.product();
-
-                            // we need to use L'Hôpital in this case
-                            let xy_close = (s.x - s.y).abs() < SINGULAR_VALUE_SEPARATION;
-                            let yz_close = (s.y - s.z).abs() < SINGULAR_VALUE_SEPARATION;
-                            let zx_close = (s.z - s.x).abs() < SINGULAR_VALUE_SEPARATION;
-
-                            let kappa = [
-                                s.x * s.x * second.m11,
-                                s.y * s.y * second.m22,
-                                s.z * s.z * second.m33,
-                                s.y * s.y
-                                    * if xy_close {
-                                        (first.x + s.x * second.m11 - s.y * second.m21) / 2. / s.x
-                                    } else {
-                                        (s.x * first.x - s.y * first.y) / (s.x * s.x - s.y * s.y)
-                                    },
-                                s.y * s.z
-                                    * if yz_close {
-                                        (first.y + s.y * second.m22 - s.z * second.m32) / 2. / s.y
-                                    } else {
-                                        (s.y * first.y - s.z * first.z) / (s.y * s.y - s.z * s.z)
-                                    },
-                                s.z * s.x
-                                    * if zx_close {
-                                        (first.z + s.x * second.m33 - s.x * second.m13) / 2. / s.z
-                                    } else {
-                                        (s.z * first.z - s.x * first.x) / (s.z * s.z - s.x * s.x)
-                                    },
-                            ]
-                            .into_iter()
-                            .max_by(T::total_cmp)
-                            .unwrap()
-                                / j;
-                            let initial_density = mass / initial_volume;
-                            let current_density = initial_density / j;
-
-                            let c = (kappa / current_density).sqrt();
-
-                            grid_node_size / c
                         }
                         ParticleParameters::Fluid {
                             exponent,
                             bulk_modulus,
-                            viscosity,
-                        } => todo!(),
-                    }
-                },
-            )
-            .min_by(T::total_cmp);
-
-        phase_input.time_step_by_isolated = self
-            .particles
-            .parameters
-            .iter()
-            .zip(self.particles.position_gradients.iter())
-            .zip(self.particles.masses.iter())
-            .zip(self.particles.initial_volumes.iter())
-            .map(
-                |(((parameters, position_gradient), mass), initial_volume)| {
-                    match parameters {
-                        ParticleParameters::Solid {
-                            mu,
-                            lambda,
-                            viscosity,
-                            sand_alpha,
+                            viscosity, // viscosity is TODO
                         } => {
-                            let xi = 3. / grid_node_size / grid_node_size;
-                            const R: T = 1.; // APIC & CPIC
-                            const K: T = 1.; // CPIC
-                            const D: T = 3.; // 3D
-                            (mass / (initial_volume * xi * (R - K / 2.) * (mu + D / 2. * lambda)))
-                                .sqrt()
+                            first = first_piola_stress_inviscid_svd_in_diagonal_space(
+                                *bulk_modulus,
+                                *exponent,
+                                &s,
+                            );
+                            second = second_derivative_inviscid_svd_in_diagonal_space(
+                                *bulk_modulus,
+                                *exponent,
+                                &s,
+                            );
                         }
-                        ParticleParameters::Fluid {
-                            exponent,
-                            bulk_modulus,
-                            viscosity,
-                        } => todo!(),
                     }
+
+                    let kappa = [
+                        s.x * s.x * second.m11,
+                        s.y * s.y * second.m22,
+                        s.z * s.z * second.m33,
+                        s.y * s.y
+                            * if xy_close {
+                                (first.x + s.x * second.m11 - s.y * second.m21) / 2. / s.x
+                            } else {
+                                (s.x * first.x - s.y * first.y) / (s.x * s.x - s.y * s.y)
+                            },
+                        s.y * s.z
+                            * if yz_close {
+                                (first.y + s.y * second.m22 - s.z * second.m32) / 2. / s.y
+                            } else {
+                                (s.y * first.y - s.z * first.z) / (s.y * s.y - s.z * s.z)
+                            },
+                        s.z * s.x
+                            * if zx_close {
+                                (first.z + s.x * second.m33 - s.x * second.m13) / 2. / s.z
+                            } else {
+                                (s.z * first.z - s.x * first.x) / (s.z * s.z - s.x * s.x)
+                            },
+                    ]
+                    .into_iter()
+                    .max_by(T::total_cmp)
+                    .unwrap()
+                        / j;
+                    let initial_density = mass / initial_volume;
+                    let current_density = initial_density / j;
+
+                    let c = (kappa / current_density).sqrt();
+
+                    grid_node_size / c
                 },
             )
-            .min_by(T::total_cmp);
+            .min_by(T::total_cmp)
+    }
 
-        // just for comparison
-        phase_input.time_step_by_sound_simple = self
-            .particles
+    // Stability analysis of explicit MPM, Technical document 3.12
+    fn limit_time_step_by_isolated_particles(&self, phase_input: &mut PhaseInput) -> Option<T> {
+        profile!("limit_time_step_by_isolated_particles");
+        let grid_node_size = phase_input.setup.settings.grid_node_size;
+        self.particles
             .parameters
             .iter()
-            .zip(self.particles.position_gradients.iter())
             .zip(self.particles.masses.iter())
             .zip(self.particles.initial_volumes.iter())
-            .map(
-                |(((parameters, position_gradient), mass), initial_volume)| match parameters {
+            .map(|((parameters, mass), initial_volume)| {
+                match parameters {
                     ParticleParameters::Solid {
                         mu,
                         lambda,
-                        viscosity,
-                        sand_alpha,
+                        viscosity,     // viscosity is TODO
+                        sand_alpha: _, // this shouldn't lower the required timestep
                     } => {
-                        let initial_density = mass / initial_volume;
-                        let current_density = initial_density / position_gradient.determinant();
-
-                        let bulk_modulus = lambda + 2. / 3. * mu;
-                        let c = (bulk_modulus / current_density).sqrt();
-
-                        grid_node_size / c
+                        let xi = 3. / grid_node_size / grid_node_size;
+                        const R: T = 1.; // APIC & CPIC
+                        const K: T = 1.; // CPIC
+                        const D: T = 3.; // 3D
+                        (mass / (initial_volume * xi * (R - K / 2.) * (mu + D / 2. * lambda)))
+                            .sqrt()
                     }
                     ParticleParameters::Fluid {
                         exponent,
                         bulk_modulus,
-                        viscosity,
-                    } => todo!(),
-                },
-            )
-            .min_by(T::total_cmp);
-
-        apply_limit(phase_input);
-        Ok(self)
+                        viscosity, // viscosity is TODO
+                    } => T::MAX, // TODO
+                }
+            })
+            .min_by(T::total_cmp)
     }
 
     // At least somewhat similar to
