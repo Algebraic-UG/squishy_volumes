d0fb2005f5eef0b04627ba4fdf76af0ea6e8772a
Vollkornaffe <vollkornaffe@gmail.com>
2025-11-02 00:19:59 +0100

    feat: implement simple end-to-end test

--- tests/end_to_end.py
+++ tests/end_to_end.py
@@ -1,4 +1,7 @@
 import bpy
+import os
+import tempfile
+import time
 import argparse
 import logging
 import urllib
@@ -12,6 +15,7 @@ from test_util import (
     extension_url,
     fetch_available_versions,
     installed_addons,
+    temp_dir_create,
     test_factory_clean,
 )
 
@@ -50,12 +54,61 @@ if __name__ == "__main__":
     else:
         URL = extension_url(args.version)
 
+    test_name = Path(__file__).stem
+    test_dir = test_name + os.sep
+
     test_factory_clean()
 
     bpy.context.preferences.system.use_online_access = True
 
     test_install(URL)
 
-    # TODO
+    bpy.ops.scene.squishy_volumes_add_simulation(cache_directory=test_dir)
+    simulation_uuid = bpy.context.scene.squishy_volumes_scene.simulations[0].uuid
+
+    default_cube = bpy.data.objects["Cube"]
+    default_cube.hide_render = True
+
+    default_cube.select_set(True)
+    bpy.context.view_layer.objects.active = default_cube
+    bpy.ops.object.squishy_volumes_add_input_object()
+
+    bpy.ops.mesh.primitive_plane_add(
+        size=6,
+        enter_editmode=False,
+        align="WORLD",
+        location=(0, 0, -2),
+        scale=(1, 1, 1),
+    )
+    bpy.ops.object.squishy_volumes_add_input_object(object_enum="Collider")
+
+    bpy.ops.scene.squishy_volumes_write_input_to_cache()
+
+    bpy.ops.scene.squishy_volumes_wait_until_finished(simulation_uuid=simulation_uuid)
+
+    bpy.ops.scene.squishy_volumes_add_output_object(
+        object_name="SOLID_PARTICLES - Cube",
+        output_type="SOLID_PARTICLES",
+        input_name="Cube",
+        num_colliders=1,
+    )
+
+    bpy.context.scene.frame_end = 100
+    bpy.context.scene.render.resolution_percentage = 20
+
+    bpy.context.scene.render.filepath = test_dir
+    bpy.context.scene.render.image_settings.media_type = "VIDEO"
+    bpy.context.scene.render.image_settings.file_format = "FFMPEG"
+    bpy.context.scene.render.ffmpeg.format = "MPEG4"
+    bpy.context.scene.render.ffmpeg.codec = "H264"
+    bpy.context.scene.render.ffmpeg.constant_rate_factor = "MEDIUM"
+    bpy.context.scene.render.ffmpeg.ffmpeg_preset = "GOOD"
+    bpy.context.scene.render.ffmpeg.gopsize = 250
+    bpy.context.scene.render.ffmpeg.use_max_b_frames = True
+    bpy.context.scene.render.ffmpeg.max_b_frames = 2
+
+    bpy.ops.render.render(animation=True)
+
+    bpy.ops.wm.save_as_mainfile(filepath=f"{test_dir}{test_name}.blend")
 
     test_uninstall(URL)
