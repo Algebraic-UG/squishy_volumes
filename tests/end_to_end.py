40f4039520afe945236ce4d0c55677136849d1db
Vollkornaffe <vollkornaffe@gmail.com>
2025-11-03 14:52:42 +0100

    chore: adding headers, cleanup

--- tests/end_to_end.py
+++ tests/end_to_end.py
@@ -1,7 +1,23 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This file is part of the Squishy Volumes extension.
+# Copyright (C) 2025  Algebraic UG (haftungsbeschränkt)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import bpy
 import os
-import tempfile
-import time
 import argparse
 import logging
 import urllib
@@ -15,7 +31,6 @@ from test_util import (
     extension_url,
     fetch_available_versions,
     installed_addons,
-    temp_dir_create,
     test_factory_clean,
 )
 
