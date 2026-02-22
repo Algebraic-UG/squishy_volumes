757e1ff84592519c21d7b49858dd2c11db167c5f
Vollkornaffe <vollkornaffe@gmail.com>
2026-02-22 01:04:09 +0100

    chore: bump version to 0.2.0

--- rust/crates/wrap/src/shim.rs
+++ rust/crates/wrap/src/shim.rs
@@ -14,6 +14,6 @@ use pyo3::prelude::*;
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
 #[pymodule]
-pub fn squishy_volumes_wrap_0_1_21(m: &Bound<'_, PyModule>) -> PyResult<()> {
+pub fn squishy_volumes_wrap_0_2_0(m: &Bound<'_, PyModule>) -> PyResult<()> {
     super::squishy_volumes_wrap(m)
 }
