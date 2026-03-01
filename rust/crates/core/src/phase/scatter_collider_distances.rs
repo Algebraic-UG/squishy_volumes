4680dc04bfbdb13a8fdd2c28ed241c350bb6b22e
Vollkornaffe <vollkornaffe@gmail.com>
2026-03-01 12:32:38 +0100

    feat: more intuitive profiling

--- rust/crates/core/src/phase/scatter_collider_distances.rs
+++ rust/crates/core/src/phase/scatter_collider_distances.rs
@@ -6,7 +6,7 @@
 // license that can be found in the LICENSE_MIT file or at
 // https://opensource.org/licenses/MIT.
 
-use std::{collections::hash_map::Entry, mem::swap, rc::Rc, sync::mpsc::channel, thread::spawn};
+use std::{collections::hash_map::Entry, sync::mpsc::channel, thread::spawn};
 
 use anyhow::{Context as _, Result, bail};
 use nalgebra::Vector3;
@@ -61,95 +61,101 @@ impl State {
                 });
         }
 
-        let (tx, rx) = channel::<(Vector3<i32>, (usize, Rasterized))>();
-        let collector = spawn(
-            move || -> FxHashMap<Vector3<i32>, FxHashMap<usize, Rasterized>> {
-                let mut new_entries: FxHashMap<Vector3<i32>, FxHashMap<usize, Rasterized>> =
-                    Default::default();
-                while let Ok((grid_idx, (collider_idx, rasterized))) = rx.recv() {
-                    match new_entries.entry(grid_idx).or_default().entry(collider_idx) {
-                        Entry::Occupied(mut occupied_entry) => {
-                            let existing = occupied_entry.get_mut();
-                            if existing.distance_abs() < rasterized.distance_abs() {
-                                continue;
-                            }
-                            *existing = rasterized;
-                        }
-                        Entry::Vacant(vacant_entry) => {
-                            vacant_entry.insert(rasterized);
-                        }
-                    }
-                }
-                new_entries
-            },
-        );
-
-        for (name, input) in collider_input.iter() {
-            let object_index = self.name_map.get(name).context("Missing object")?;
-            let ObjectIndex::Collider(collider_index) = object_index.clone() else {
-                bail!("Wrong object type");
-            };
-
-            let make_rasterization_vertex = |index: usize| RasterizationVertex {
-                position: &input.vertex_positions[index],
-                velocity: &input.vertex_velocities[index],
-                normal: &input.vertex_normals[index],
-            };
-
-            input
-                .triangles
-                .par_iter()
-                .zip(&input.triangle_frictions)
-                .for_each(|(&[a, b, c], friction)| {
-                    let order_edge = |[a, b]: [u32; 2]| if a < b { [a, b] } else { [b, a] };
-                    let pick_other = |a: u32| {
-                        move |&[b, c]: &[u32; 2]| {
-                            &input.vertex_positions[if b != a { b } else { c } as usize]
-                        }
-                    };
-                    let opposite_d = input
-                        .edges_with_opposites
-                        .get(&order_edge([a, b]))
-                        .map(pick_other(c));
-                    let opposite_e = input
-                        .edges_with_opposites
-                        .get(&order_edge([b, c]))
-                        .map(pick_other(a));
-                    let opposite_f = input
-                        .edges_with_opposites
-                        .get(&order_edge([c, a]))
-                        .map(pick_other(b));
-
-                    for (grid_idx, rasterized) in rasterize(
-                        grid_node_size,
-                        RASTERIZATION_LAYERS,
-                        [
-                            make_rasterization_vertex(a as usize),
-                            make_rasterization_vertex(b as usize),
-                            make_rasterization_vertex(c as usize),
-                        ],
-                        [opposite_d, opposite_e, opposite_f],
-                        *friction,
-                    ) {
-                        let Some(grid_node) = self.grid_collider.get(&grid_idx) else {
-                            tx.send((grid_idx, (collider_index, rasterized)))
-                                .expect("collider collector died");
-                            continue;
-                        };
-
-                        match grid_node.assume_mut().lock().entry(collider_index) {
+        let collector = {
+            profile!("scatter");
+            let (tx, rx) = channel::<(Vector3<i32>, (usize, Rasterized))>();
+            let collector = spawn(
+                move || -> FxHashMap<Vector3<i32>, FxHashMap<usize, Rasterized>> {
+                    let mut new_entries: FxHashMap<Vector3<i32>, FxHashMap<usize, Rasterized>> =
+                        Default::default();
+                    while let Ok((grid_idx, (collider_idx, rasterized))) = rx.recv() {
+                        match new_entries.entry(grid_idx).or_default().entry(collider_idx) {
                             Entry::Occupied(mut occupied_entry) => {
-                                if occupied_entry.get().distance_abs() > rasterized.distance_abs() {
-                                    occupied_entry.insert(rasterized);
+                                let existing = occupied_entry.get_mut();
+                                if existing.distance_abs() < rasterized.distance_abs() {
+                                    continue;
                                 }
+                                *existing = rasterized;
                             }
                             Entry::Vacant(vacant_entry) => {
                                 vacant_entry.insert(rasterized);
                             }
                         }
                     }
-                });
-        }
+                    new_entries
+                },
+            );
+
+            for (name, input) in collider_input.iter() {
+                let object_index = self.name_map.get(name).context("Missing object")?;
+                let ObjectIndex::Collider(collider_index) = object_index.clone() else {
+                    bail!("Wrong object type");
+                };
+
+                let make_rasterization_vertex = |index: usize| RasterizationVertex {
+                    position: &input.vertex_positions[index],
+                    velocity: &input.vertex_velocities[index],
+                    normal: &input.vertex_normals[index],
+                };
+
+                input
+                    .triangles
+                    .par_iter()
+                    .zip(&input.triangle_frictions)
+                    .for_each(|(&[a, b, c], friction)| {
+                        let order_edge = |[a, b]: [u32; 2]| if a < b { [a, b] } else { [b, a] };
+                        let pick_other = |a: u32| {
+                            move |&[b, c]: &[u32; 2]| {
+                                &input.vertex_positions[if b != a { b } else { c } as usize]
+                            }
+                        };
+                        let opposite_d = input
+                            .edges_with_opposites
+                            .get(&order_edge([a, b]))
+                            .map(pick_other(c));
+                        let opposite_e = input
+                            .edges_with_opposites
+                            .get(&order_edge([b, c]))
+                            .map(pick_other(a));
+                        let opposite_f = input
+                            .edges_with_opposites
+                            .get(&order_edge([c, a]))
+                            .map(pick_other(b));
+
+                        for (grid_idx, rasterized) in rasterize(
+                            grid_node_size,
+                            RASTERIZATION_LAYERS,
+                            [
+                                make_rasterization_vertex(a as usize),
+                                make_rasterization_vertex(b as usize),
+                                make_rasterization_vertex(c as usize),
+                            ],
+                            [opposite_d, opposite_e, opposite_f],
+                            *friction,
+                        ) {
+                            let Some(grid_node) = self.grid_collider.get(&grid_idx) else {
+                                tx.send((grid_idx, (collider_index, rasterized)))
+                                    .expect("collider collector died");
+                                continue;
+                            };
+
+                            match grid_node.assume_mut().lock().entry(collider_index) {
+                                Entry::Occupied(mut occupied_entry) => {
+                                    if occupied_entry.get().distance_abs()
+                                        > rasterized.distance_abs()
+                                    {
+                                        occupied_entry.insert(rasterized);
+                                    }
+                                }
+                                Entry::Vacant(vacant_entry) => {
+                                    vacant_entry.insert(rasterized);
+                                }
+                            }
+                        }
+                    });
+            }
+            collector
+        };
 
         {
             profile!("transition");
@@ -162,7 +168,6 @@ impl State {
 
         {
             profile!("extend");
-            drop(tx);
             let new_entries = collector.join().unwrap();
             self.grid_collider.par_extend(
                 new_entries
