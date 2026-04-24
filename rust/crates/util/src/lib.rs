f49e032a0a4e14a6b0fa5fe4580c33b12333add0
Vollkornaffe <vollkornaffe@gmail.com>
2026-04-24 19:10:37 +0200

    move common stuff to util crate

---
+++ rust/crates/util/src/lib.rs
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright 2025  Algebraic UG (haftungsbeschränkt)
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE_MIT file or at
+// https://opensource.org/licenses/MIT.
+
+mod aabb;
+mod consts;
+mod elastic;
+pub mod flat;
+pub mod safe_inverse;
+mod typedefs;
+
+pub use aabb::Aabb;
+
+pub use consts::*;
+pub use elastic::*;
+pub use typedefs::*;
+
+#[cfg(test)]
+type T = f64;
+#[cfg(not(test))]
+type T = f32;
+
+#[cfg(test)]
+mod tests;
+
+#[macro_export]
+macro_rules! ensure_err {
+    ($cond:expr, $err:expr $(,)?) => {
+        #[allow(clippy::neg_cmp_op_on_partial_ord)]
+        if !$cond {
+            return Err($err);
+        }
+    };
+}
