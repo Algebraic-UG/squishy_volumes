cfbbbb64548fa77059304f7184854ebd3f456d2f
Vollkornaffe <vollkornaffe@gmail.com>
2026-07-02 18:04:38 +0200

    remove old approach 'gpu_with_blocks'

--- rust/crates/gpu_with_blocks_cli/scripts/test_vs_cpu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TASK="$1"
-
-cargo run --release --bin squishy_volumes_gpu_cli -- cpu "$TASK" --generate 1000000
-cargo run --release --bin squishy_volumes_gpu_cli -- gpu "$TASK" 
-
-diff "test_data/cpu-$TASK-out.bin" "test_data/gpu-$TASK-out.bin"
