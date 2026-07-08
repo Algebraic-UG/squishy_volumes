bd1b4d75c014476df6755437e62c263c03026cea
Vollkornaffe <vollkornaffe@gmail.com>
2026-07-08 16:56:17 +0200

    the prior timestep shouldn't take the ones before into account

--- rust/crates/cpu/src/adaptive_time_step_state.rs
+++ rust/crates/cpu/src/adaptive_time_step_state.rs
@@ -6,6 +6,8 @@
 // license that can be found in the LICENSE_MIT file or at
 // https://opensource.org/licenses/MIT.
 
+use std::iter::once;
+
 const TIME_STEP_HISTORY_LENGTH: usize = 10;
 
 pub struct AdaptiveTimeStepState {
@@ -31,7 +33,7 @@ impl Default for AdaptiveTimeStepState {
 }
 
 impl AdaptiveTimeStepState {
-    pub fn allowed_time_step(&self) -> f32 {
+    fn allowed_time_step_without_prior(&self) -> f32 {
         [
             self.max_time_step,
             self.time_step_by_velocity.unwrap_or(f32::MAX),
@@ -40,9 +42,15 @@ impl AdaptiveTimeStepState {
             self.time_step_by_isolated.unwrap_or(f32::MAX),
         ]
         .into_iter()
-        .chain(self.time_step_prior.iter().cloned())
         .min_by(f32::total_cmp)
         .unwrap()
+    }
+
+    pub fn allowed_time_step(&self) -> f32 {
+        once(self.allowed_time_step_without_prior())
+            .chain(self.time_step_prior.iter().cloned())
+            .min_by(f32::total_cmp)
+            .unwrap()
     }
 
     pub fn push_current_limit(&mut self) {
@@ -50,6 +58,7 @@ impl AdaptiveTimeStepState {
             self.time_step_prior.pop_front();
         }
 
-        self.time_step_prior.push_back(self.allowed_time_step());
+        self.time_step_prior
+            .push_back(self.allowed_time_step_without_prior());
     }
 }
