19fdc6043af84c9895541b7d38c5d9879ff45f36
Vollkornaffe <vollkornaffe@gmail.com>
2026-07-11 22:35:56 +0200

    fix rebuild zip script

--- scripts/rebuild_zip.sh
+++ scripts/rebuild_zip.sh
@@ -2,22 +2,27 @@
 
 set -e
 
-cd "$(dirname "$0")/.."
+scripts_dir=$(dirname $(realpath "$0"))
+blender_ext_file="${scripts_dir}/blender_ext.py"
+repo_root=$(dirname "${scripts_dir}")
+wrap_dir="${repo_root}/rust/crates/wrap"
+extension_dir="${repo_root}/python/src/squishy_volumes_extension"
+wheels_dir="${extension_dir}/wheels"
 
-rm -f python/wheels/*
+rm -i "${wheels_dir}/*" || true
 
-uv run --with toml scripts/update_lib_version.py
+uv run --with toml "${scripts_dir}/update_lib_version.py"
 
-./scripts/add_license_headers.sh
+"${scripts_dir}/add_license_headers.sh"
 
-cd rust/crates/wrap
-uvx --python 3.11 maturin build --no-default-features --release --out ../../../python/src/squishy_volumes_extension/wheels/
+cd "${wrap_dir}"
+uvx --python 3.11 maturin build --no-default-features --release --out "${wheels_dir}"
 
 cd -
-uv run --with toml scripts/update_manifest.py
+uv run --with toml "${scripts_dir}/update_manifest.py"
 
-test -f ./scripts/blender_ext.py || wget https://raw.githubusercontent.com/blender/blender/refs/tags/v4.5.0/scripts/addons_core/bl_pkg/cli/blender_ext.py --output-document=./scripts/blender_ext.py
+test -f "${blender_ext_file}" || wget https://raw.githubusercontent.com/blender/blender/refs/tags/v4.5.0/scripts/addons_core/bl_pkg/cli/blender_ext.py --output-document="${blender_ext_file}"
 
-cd python
-uv run --python 3.11 ../scripts/blender_ext.py build
-uv run --python 3.11 ../scripts/blender_ext.py server-generate --repo-dir=./
+cd "${extension_dir}"
+uv run --python 3.11 "${blender_ext_file}" build
+uv run --python 3.11 "${blender_ext_file}" server-generate --repo-dir=./
