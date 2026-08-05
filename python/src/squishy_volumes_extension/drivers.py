584128bff029b9249b04f19f681cbfa088e40f54
Vollkornaffe <vollkornaffe@gmail.com>
2026-08-05 13:51:25 +0200

    feat: add operator to (re)establish drivers

--- python/src/squishy_volumes_extension/drivers.py
+++ python/src/squishy_volumes_extension/drivers.py
@@ -18,6 +18,12 @@
 
 import bpy
 
+from .squishy_volumes_properties import (
+    TYPE_INPUT,
+    TYPE_OUTPUT,
+    get_simulation_object_with_uuid,
+)
+
 
 def add_drivers(sim_obj, modifier):
     tree = modifier.node_group.interface.items_tree
@@ -42,3 +48,67 @@ def add_drivers(sim_obj, modifier):
         target.data_path = "squishy_volumes.grid_node_size"
         target.id_type = "OBJECT"
         target.id = sim_obj
+
+
+class OBJECT_OT_Squishy_Volumes_Input_Object_Add_Drivers(bpy.types.Operator):
+    bl_idname = "object.squishy_volumes_add_drivers"
+    bl_label = "Add Drivers"
+    bl_description = """This adds drivers to the objects' modifiers' sockets.
+
+Only changes active input/output objects.
+Only changes modifiers starting with 'Squishy Volmues'.
+
+This connects the respective value, for example, 'Grid Node Size', with the
+value of the respective simulation(s) the objects are an output of."""
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+        for obj in context.selected_objects:
+            props = obj.squishy_volumes
+            if props.type not in [TYPE_OUTPUT, TYPE_INPUT]:
+                continue
+
+            sim_obj = get_simulation_object_with_uuid(props.uuid)
+            if sim_obj is None:
+                continue
+
+            for modifier in obj.modifiers:
+                if not modifier.name.startswith("Squishy Volumes"):
+                    continue
+                add_drivers(sim_obj, modifier)
+
+            self.report(
+                {"INFO"}, f"Added driver to modifier {modifier.name} of {obj.name}."
+            )
+
+        return {"FINISHED"}
+
+
+classes = [
+    OBJECT_OT_Squishy_Volumes_Input_Object_Add_Drivers,
+]
+
+
+def menu_func_add_goals(self, _context):
+    self.layout.operator(
+        OBJECT_OT_Squishy_Volumes_Input_Object_Add_Drivers.bl_idname,
+        icon="MODIFIER",
+    )
+
+
+menu_funcs = [menu_func_add_goals]
+
+
+def register_drivers():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    for menu_func in menu_funcs:
+        bpy.types.VIEW3D_MT_object.append(menu_func)
+
+
+def unregister_drivers():
+    for menu_func in menu_funcs:
+        bpy.types.VIEW3D_MT_object.remove(menu_func)
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
