eb45fe0d2505606afa0c249e42053c31557bff95
Vollkornaffe <vollkornaffe@gmail.com>
2026-08-18 23:16:47 +0200

    feat: prepare for flag merge

--- rust/crates/gpu/src/lib.rs
+++ rust/crates/gpu/src/lib.rs
@@ -73,6 +73,7 @@ pub mod scatter;
 pub mod step;
 pub mod test_status;
 pub mod test_svd;
+pub mod update_flags;
 pub mod viscosity;
 
 pub use animate_mesh::AnimateMesh;
@@ -99,6 +100,7 @@ pub use scatter::Scatter;
 pub use step::Step;
 pub use test_status::TestStatus;
 pub use test_svd::TestSvd;
+pub use update_flags::UpdateFlags;
 pub use viscosity::Viscosity;
 
 #[cfg(test)]
